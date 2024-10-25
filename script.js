e404b70b5c6a6654927f5b15d169f0b3843a4e99
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-10-25 16:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function isSameType(value1, value2) {
+  // Convert inputs to numbers if they are numeric strings
+  value1 = isNaN(value1) ? value1 : Number(value1);
+  value2 = isNaN(value2) ? value2 : Number(value2);
+
   // Check if both values are NaN
   if (Number.isNaN(value1) && Number.isNaN(value2)) {
     return true;
   }
-  
+
   // Check if the types are the same
-  return typeof value1 === typeof value2	
+  return typeof value1 === typeof value2;
 }
 
-// do not change the code below.
+// Do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 alert(isSameType(value1, value2));
-
